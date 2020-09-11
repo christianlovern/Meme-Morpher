8d3e3f0696de24324e75f72485e00b2d593b1b1d
Christian <lovernchristian@gmail.com>
2020-09-10 21:31:33 -0400

    Sponge-bird added

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 window.addEventListener('DOMContentLoaded', event => {
     let textBox = document.querySelector('.textbox')
-    let submitBut = document.getElementById('submit')
+    let vowelBut = document.getElementById('vowels')
     let textBoxFinal = document.querySelector('.textbox-final')
-    submitBut.addEventListener('click', event => {
+    let spongeBut = document.getElementById('sponge')
+    vowelBut.addEventListener('click', event => {
         let result = vowelRemover(textBox.value);
         textBoxFinal.innerHTML = result;
     })
+
+    spongeBut.addEventListener('click', event => {
+        console.log('hello')
+        let result = spongeMock(textBox.value);
+        textBoxFinal.innerHTML = result;
+    })
+
+
+
+
+    function toggleCase(str) {
+        if (str.length !== 1) return str;
+
+        if (str.match(/^[A-z]$/)) {
+          if (str.toUpperCase() === str) {
+            return str.toLowerCase();
+          } else {
+            return str.toUpperCase();
+          }
+        }
+
+        return str;
+      }
+
+    function spongeMock(str) {
+        return str
+          .split("")
+          .map((char, index) => {
+            if (index % 2 === 0) return toggleCase(char);
+            return char;
+          })
+          .join("");
+      }
+
 
 
     function vowelRemover(sentence){
